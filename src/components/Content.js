928aacf520c0a88be0ef255c4b28d96d3c099607
gouravthakur39 <gourav.thakur39@gmail.com>
2021-11-07 20:39:15 +0530

    options footer, question container

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,7 @@ import "./Content.css";
 const Content = (props) => {
   const { subjectName } = props;
   const [displayTopic, setDisplayTopic] = useState([]);
-  console.log('display topic :',displayTopic)
+  console.log("display topic :", displayTopic);
 
   let displayTopicAccordingtoSubject = displayTopic.filter(
     (item) => item.subject === subjectName
@@ -28,22 +28,58 @@ const Content = (props) => {
         </div>
         <Row className="topic-lists-and-control">
           <div className="topic-dropdown-container">
-            {displayTopic.length !== 0 && <select name="topic" id="topic" className="topic-dropdown">
-              <option value="" selected>
-                -- Choose a topic to practice --
-              </option>
-              {displayTopicAccordingtoSubject.map((item, index) => {
-              return (
-                  <option key={index} value={item.topic}>
-                    {item.topic}
-                  </option>
-                );
-              })}
-            </select>}
+            {displayTopic.length !== 0 && (
+              <select name="topic" id="topic" className="topic-dropdown">
+                <option value="" selected>
+                  -- Choose a topic to practice --
+                </option>
+                {displayTopicAccordingtoSubject.map((item, index) => {
+                  return (
+                    <option key={index} value={item.topic}>
+                      {item.topic}
+                    </option>
+                  );
+                })}
+              </select>
+            )}
           </div>
           <div className="prev-next-btn">
             <Button>Prev ⏮️</Button>
             <Button>Next ⏭️</Button>
+          </div>
+        </Row>
+        <Row className="question-container">
+          <strong>What is Lorem Ipsum?</strong>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. Why do we use it? It is
+            a long established fact that a reader will be distracted by the
+            readable content of a page when looking at its layout. The point of
+            using Lorem Ipsum is that it has a more-or-less normal distribution
+            of letters, as opposed to using 'Content here, content here', making
+            it look like readable English. Many desktop publishing packages and
+            web page editors now use Lorem Ipsum as their default model text,
+            and a search for 'lorem ipsum' will uncover many web sites still in
+            their infancy. Various versions have evolved over the years,
+            sometimes by accident, sometimes on purpose (injected humour and the
+            like). 
+            </p>
+        </Row>
+        <Row className="settings-footer">
+          <div className="options">
+            <p>⚙️ Settings</p>
+            <p>📈 Insights</p>
+            <p>💡 Solution</p>
+            <p>🤝 Discuss</p>
+            <p>🐞 Report a bug</p>
+            <p>✍️ Request a feature</p>
           </div>
         </Row>
       </Col>
